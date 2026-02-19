f68b074d0f418ab23424b93c725c8721b77c9aee
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-02-19 16:10:59 -0700

    Create parts array

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -3,57 +3,44 @@ const Header = (props) => {
     <h1>{props.course}</h1>
   )
 }
-const Content = () => {
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-  
+const Content = (props) => {
   return (
     <div>
-      <Part part={part1} exercise={exercises1}/>
-      <Part part={part2} exercise={exercises2}/>
-      <Part part={part3} exercise={exercises3}/>
+      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
     </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.part} {props.exercise}</p>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exerciseTotal}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const sumExercises = part1.exercises + part2.exercises + part3.exercises;
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total exerciseTotal={sumExercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
